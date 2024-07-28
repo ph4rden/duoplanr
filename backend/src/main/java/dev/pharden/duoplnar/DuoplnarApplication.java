d195eb67e6b52b225b2a3dfd64cc9d57958403ef
Brian <brianphan17@yahoo.com>
2024-07-28 15:54:35 -0500

    project setup

---
+++ backend/src/main/java/dev/pharden/duoplnar/DuoplnarApplication.java
@@ -0,0 +1,13 @@
+package dev.pharden.duoplnar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DuoplnarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DuoplnarApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/dev/pharden/duoplnar/DuoplnarApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.pharden.duoplnar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DuoplnarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
